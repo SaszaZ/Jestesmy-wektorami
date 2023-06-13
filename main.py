18c3a0cbb4a9796e2c148c1f471940e19eeaeada
SaszaZ <136440587+SaszaZ@users.noreply.github.com>
2023-06-13 18:43:53 +0200

    Stworzenie skryptu do instalacji

---
+++ main.py
@@ -0,0 +1,34 @@
+import cv2
+import matplotlib.pyplot as plt
+import random
+import numpy as np
+
+vid = cv2.VideoCapture(0)
+
+_rand = False
+
+def plot(frame, _rand):
+    x, y = np.meshgrid(np.linspace(-5, 5, 960), np.linspace(-5, 5, 960))
+    if _rand:
+        frame*=random.randint(1,10)
+    fig, ax = plt.subplots(1,1)
+    qr = ax.quiver(x,y,frame)
+    plt.savefig('picplot.png')
+    plt.close()
+
+
+while (True):
+    ret, frame = vid.read()
+    x, y = np.meshgrid(np.linspace(-5, 5, 960), np.linspace(-5, 5, 960))
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('r'):
+        _rand = not _rand
+    if cv2.waitKey(1) & 0xFF == ord('p'):
+        plot(frame, _rand)
+        image= cv2.imread('picplot.png')
+        cv2.imshow('plot', image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+vid.release()
+cv2.destroyAllWindows()
