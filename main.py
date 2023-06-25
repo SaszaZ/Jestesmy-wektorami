9839e49fe0de3f6c20c7a1693fc410b364d177e0
SaszaZ <136440587+SaszaZ@users.noreply.github.com>
2023-06-25 23:10:23 +0200

    Update main.py
    Change frame behaviour

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ _rand = False
 
 def plot(frame, _rand):
     x, y = np.meshgrid(np.linspace(-5, 5, 960), np.linspace(-5, 5, 960))
-    if _rand:
-        frame*=random.randint(1,10)
+
+    # if _rand:
+    #     frame*=random.randint(1,10)
     fig, ax = plt.subplots(1,1)
-    qr = ax.quiver(x,y,frame)
+    qr = ax.quiver(x,y,frame1)
     plt.savefig('picplot.png')
     plt.close()
 
@@ -21,12 +22,18 @@ while (True):
     ret, frame = vid.read()
     x, y = np.meshgrid(np.linspace(-5, 5, 960), np.linspace(-5, 5, 960))
     cv2.imshow('To Ty!', frame)
-    if cv2.waitKey(1) & 0xFF == ord('r'):
-        _rand = not _rand
+    # if cv2.waitKey(1) & 0xFF == ord('r'):
+    #     _rand = not _rand
     if cv2.waitKey(1) & 0xFF == ord('p'):
-        plot(frame, _rand)
+        if 'frame1' in locals():
+            frame1 += frame
+        elif 'frame1' in globals():
+            frame1 += frame
+        else:
+            frame1 = frame
+        plot(frame1, _rand)
         image= cv2.imread('picplot.png')
-        cv2.imshow('Jestes wektorem!', image)
+        cv2.imshow('Jestes wektorami!', image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
